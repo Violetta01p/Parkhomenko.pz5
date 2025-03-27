af3bf08ce11050ac1b8b412c45df2fde6acf769c
Violetta01p <pvsergeevnaa@gmail.com>
2025-03-27 10:56:29 +0200

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter temperature: "))
+b = int(input("Enter humidity: "))
+
+
+if a >= 30 and b >= 70:
+   print("Activation of the cooling system")
+else:
+   print("Conditions are normal")
